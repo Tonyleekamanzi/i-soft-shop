bcd45eeebdc77a9f4aa15c4e097c03fba350229d
DEV kamanzi tony <kamanzitony06@gmail.com>
2025-04-13 19:54:11 +0200

    creating women part.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import "./App.css";
-import AllProducts from "./components/products";
 import data from "./data/products.js";
 import Header from "./components/Header";
+import AllProducts from "./components/products";
 import Men from "./components/Men.jsx";
+import WomenProduct from "./components/Women.jsx";
 
 export default function App() {
   const productElement = data.map((product) => {
@@ -13,11 +14,17 @@ export default function App() {
     .filter((product) => product.keywords.includes("mens"))
     .map((product) => <Men key={product.id} {...product} />);
 
+    const womenProducts = data
+    .filter((product) => product.keywords.includes("womens"))
+    .map((product) => <WomenProduct key={product.id} {...product} />);
+
   return (
     <>
       <Header />
       <h1>Mens</h1>
-      <div className="container">{menProducts}</div>  
+      <div className="container">{menProducts}</div>
+      <h1>Womens</h1>
+      <div className="container">{womenProducts}</div>
       <h1>All Products</h1>
       <div className="container">{productElement}</div>
     </>
