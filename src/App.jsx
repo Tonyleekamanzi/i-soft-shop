e3fc7cf79b3f10658d598b55c8c6c7b8f02f77b0
DEV kamanzi tony <kamanzitony06@gmail.com>
2025-04-13 19:46:31 +0200

    creating the men part

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,23 @@ import "./App.css";
 import AllProducts from "./components/products";
 import data from "./data/products.js";
 import Header from "./components/Header";
+import Men from "./components/Men.jsx";
 
 export default function App() {
   const productElement = data.map((product) => {
-    return (
-    <AllProducts 
-    key={product.id} 
-    {...product} 
-    />
-  )
-  })
+    return <AllProducts key={product.id} {...product} />;
+  });
+
+  const menProducts = data
+    .filter((product) => product.keywords.includes("mens"))
+    .map((product) => <Men key={product.id} {...product} />);
 
   return (
     <>
       <Header />
+      <h1>Mens</h1>
+      <div className="container">{menProducts}</div>  
+      <h1>All Products</h1>
       <div className="container">{productElement}</div>
     </>
   );
